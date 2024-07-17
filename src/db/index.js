6b10f2fe3d22974e61d5d776537e6ea6cb23f257
Soumay-Sanpui <somusanpui9@gmail.com>
2024-07-17 12:12:14 +0530

    updated db config

--- src/db/index.js
+++ src/db/index.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose';
-import {DB_NAME} from "../../constants.js";
+import {DB_NAME, MONGOURI} from "../../constants.js";
 let isConnected = false;
 const connectToMongoDB = async () => {
     try {
         if(!isConnected) {
-            await mongoose.connect("mongodb+srv://somu:somu@cluster0.1f4iwcz.mongodb.net/",{dbName: DB_NAME});
+            await mongoose.connect(MONGOURI,{dbName: DB_NAME});
             isConnected = !isConnected;
         }
     } catch (error) {
